4b80da1b2ff136245f0e3676d9dfb599eca668fd
Iago <igl2005@hotmail.com>
2026-05-04 21:55:33 +0200

    feat: configure gh-pages deployment scripts and update repository base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Si tu repo es 'https://github.com/usuario/mi-repo', cambia el base a: '/mi-repo/'
-  base: './', 
+  base: '/minecraftServerWeb/', 
 })
